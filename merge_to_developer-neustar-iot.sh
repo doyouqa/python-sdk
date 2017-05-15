8086e9a2a9332a11b93c818ac5b10479c9ad3e26
Holderman <callie.holderman@neustar.biz>
2017-05-15 10:49:43 -0700

    Changes script to simple print-line test.

--- merge_to_developer-neustar-iot.sh
+++ merge_to_developer-neustar-iot.sh
@@ -1,7 +1,14 @@
 # Need this to happen non-locally: in the cloud, in docker?
-git clone https://github.com/OneID/developer-neustar-iot.git
-cd developer-neustar-iot
-git submodule update --remote oneid-connect-python #update
-git add .
-git commit -m "Pulls in Python API documentation"
-git push
+
+echo -e "###   ... ... ... ... ... ... ...    ###"
+echo -e "###   ... ... ... ... ... ... ...   ###"
+echo -e "###   THE UPDATE SCRIPT IS CALLED   ###"
+echo -e "###   ... ... ... ... ... ... ...   ###"
+echo -e "###   ... ... ... ... ... ... ...   ###"
+
+# git clone https://github.com/OneID/developer-neustar-iot.git
+# cd developer-neustar-iot
+# git submodule update --remote oneid-connect-python #update
+# git add .
+# git commit -m "Pulls in Python API documentation"
+# git push
