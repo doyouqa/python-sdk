e47c77bf7d2cd1716c9263c7f9bf7274c3a6ea8a
Carl Dunham <cdunham@gmail.com>
2017-10-10 14:32:59 -0400

    chore: version update

--- src/oneid/__about__.py
+++ src/oneid/__about__.py
@@ -11,7 +11,7 @@ __summary__ = ("Neustar TDI is an identity & authentication "
 
 __uri__ = "https://github.com/OneID/oneID-connect-python"
 
-__version__ = "0.21.0-dev1"
+__version__ = "0.21.0"
 
 __author__ = "Neustar Inc."
 __email__ = "support@oneID.com"
